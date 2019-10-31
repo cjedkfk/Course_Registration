11a61ac47fe3a479726f182eb64f048384e1e465
kyunjun98 <kyunjun98@naver.com>
2019-10-31 10:53:59 +0900

    컨트롤러 입력과 조회단을 수정

--- test-STS/0926/src/main/java/com/example/demo/HeloController.java
+++ test-STS/0926/src/main/java/com/example/demo/HeloController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,22 +21,22 @@ public class HeloController {
     *
     * @fn 		public ModelAndView index(ModelAndView mav) 
     * 
-    * @brief 	���옣�맂 �젙蹂대�� 蹂� �닔 �엳�뒗 �럹�씠吏�
+    * @brief 	저장된 정보를 볼 수 있는 페이지
     *
-    * @author 	理쒖���
+    * @author 	최지은
     * @date 	2019-10-09
     *
     * @param 	RequestMapping ModelAndView mav
     *
-    * @remark   index�럹�씠吏�瑜� 遺덈윭�샂
-    * 			���옣�릺�뼱 �엳�뒗 媛믪쓣 紐⑤몢 遺덈윭�샂
+    * @remark   index페이지를 불러옴
+    * 			저장되어 있는 값을 모두 불러옴
     */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index(ModelAndView mav) 
     {
         mav.setViewName("index");
         mav.addObject("title","Find Page");
-        mav.addObject("msg","MyDataMongo�쓽 �삁�젣�엯�땲�떎.");
+        mav.addObject("msg","MyDataMongo의 예제입니다.");
         Iterable<MyDataMongo> list = repository.findAll();
         mav.addObject("datalist", list);
         return mav;
@@ -45,15 +46,15 @@ public class HeloController {
     *
     * @fn 		public ModelAndView insert(ModelAndView mav) 
     * 
-    * @brief 	�젙蹂대�� �엯�젰�븯�뒗 �럹�씠吏�
+    * @brief 	정보를 입력하는 페이지
     *
-    * @author 	理쒖���
+    * @author 	최지은
     * @date 	2019-10-09
     *
     * @param 	RequestMapping ModelAndView mav
     *
-    * @remark   insert�럹�씠吏�瑜� 遺덈윭�샂
-    * 			name,mail,tel媛믩뱾�쓣 DB�뿉 ���옣
+    * @remark   insert페이지를 불러옴
+    * 			name,mail,tel값들을 DB에 저장
     */
     @RequestMapping(value = "/insert", method = RequestMethod.GET)
     public ModelAndView insert(ModelAndView mav) 
@@ -68,15 +69,16 @@ public class HeloController {
 		            @RequestParam("name") String name,
 		            @RequestParam("mail") String mail,
 		            @RequestParam("tel") String tel,
+		            @RequestParam("check") String check,
 		            ModelAndView mov) 
     {
 	   
-    	 MyDataMongo mydata = new MyDataMongo(name,mail,tel);
+    	 MyDataMongo mydata = new MyDataMongo(name,mail,tel,check);
          repository.save(mydata);
          return new ModelAndView("redirect:/");
 	    
     }
-    
+
     /**
 	 *
 	 * @fn 		public ModelAndView detail(ModelAndView mav)
@@ -91,6 +93,7 @@ public class HeloController {
 	 * @remark	findBy 를 이용한 조건검색 후 출력	[2019-10-24; 권연준] \n
 	 *
 	 */
+    
 	@RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
 	public ModelAndView detail(@PathVariable("id") String id, ModelAndView mav) {
 		mav.setViewName("detail");
